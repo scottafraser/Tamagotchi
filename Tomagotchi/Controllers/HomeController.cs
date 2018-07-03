6d1874fac6e95464027f8294d214fe14c8650f5b
Scott Fraser and Thad Donaghue <scott@>
2018-07-03 10:29:44 -0700

    route through home form, game, feed, sleep, play buttons

--- Tomagotchi/Controllers/HomeController.cs
+++ Tomagotchi/Controllers/HomeController.cs
@@ -4,6 +4,38 @@ namespace TamagotchiGame.Controllers
 {
     public class HomeController : Controller
     {
-        public string Hello() { return "Hello friend!"; }
+        [HttpGet("/")]
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpGet("/game")]
+        public ActionResult Game()
+        {
+            return View();
+        }
+
+        [HttpGet("/feed")]
+        public ActionResult Feed()
+        {
+            return View();
+        }
+
+        [HttpGet("/play")]
+        public ActionResult Play()
+        {
+            return View();
+        }
+
+        [HttpGet("/sleep")]
+        public ActionResult Sleep()
+        {
+            return View();
+        }
+
+
+
+
     }
 }
---
+++ Tomagotchi/Models/Item.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace Tomagotchi.Models
+{
+    public class Item
+    {
+        private string _name;
+        private int _health;
+        private int _hunger;
+        private int _sleep;
+        private int _play;
+
+        public Item (string name)
+        {
+            _name = name;
+            _health = 100;
+            _hunger = 100;
+            _sleep = 100;
+            _play = 100;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string newName)
+        {
+            _name = newName;
+        }
+
+        public int GetHealth()
+        {
+            return _health;
+        }
+
+        public void SetHealth(int newHealth)
+        {
+            _health = newHealth;
+        }
+        public int GetHunger()
+        {
+            return _hunger;
+        }
+
+        public void SetHunger(int newHunger)
+        {
+            _hunger = newHunger;
+        }
+
+        public int GetSleep()
+        {
+            return _sleep;
+        }
+
+        public void SetSleep(int newSleep)
+        {
+            _sleep = newSleep;
+        }
+
+        public int GetPlay()
+        {
+            return _play;
+        }
+
+        public void SetPlay(int newPlay)
+        {
+            _play = newPlay;
+        }
+
+    }
+}
